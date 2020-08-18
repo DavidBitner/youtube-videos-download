286ca75a8ec898615cfe3cd5d67b07c73d23d25e
David <david-bitner@hotmail.com>
2020-08-18 04:48:06 -0300

    Update main.py
    Temporary bypass of the directory folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pytube
 
-directory = input('Input the directory were the videos are going to: ')
+directory = 'C:/Users/David/Desktop/Videos'  # input('Input the directory were the videos are going to: ')
 while True:
     video_url = input('Youtube URL: [0 to end] ')
     if video_url == '0':
