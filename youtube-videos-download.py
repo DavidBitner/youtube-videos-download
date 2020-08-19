bdd5bf5d54d104374f27faad72b4e4829da90f4c
David <david-bitner@hotmail.com>
2020-08-19 17:11:54 -0300

    Update youtube-videos-download.py

--- youtube-videos-download.py
+++ youtube-videos-download.py
@@ -22,16 +22,16 @@ def input_video_url():
 vu = input_video_url()
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=200, width=200, bg='black')
+canvas = tk.Canvas(root, height=300, width=300, bg='black')
 canvas.pack()
 
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-youtube_url_label = tk.Label(root, text='Youtube URL: ')
+youtube_url_label = tk.Label(frame, text='Youtube URL: ', padx=10, pady=5, bg='gray')
 youtube_url_label.pack()
-entry = tk.Entry(root)
-entry.pack()
+test_label = tk.Label(frame, text=vu, bg='blue')
+test_label.pack()
 
 download = tk.Button(root, text='Download', padx=10, pady=5, fg='white', bg='black', command=download_video())
 download.pack()
