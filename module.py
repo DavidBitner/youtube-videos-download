fbbaf8bad48b02889c788c64a0cd4bbaf6af76ff
David <david-bitner@hotmail.com>
2020-08-19 16:02:43 -0300

    Create module.py

---
+++ module.py
@@ -0,0 +1,18 @@
+import pytube
+
+
+def choose_directory():
+    directory = directory = input('Input the directory were the videos are going to: ')
+    return directory
+
+
+def download(directory='C:/Users/David/Desktop/Videos'):
+    while True:
+        video_url = input('Youtube URL: [0 to end] ')
+        if video_url == '0':
+            break
+        youtube = pytube.YouTube(video_url)
+        print(f'Downloading: {youtube.title}')
+        video = youtube.streams.get_highest_resolution()
+        video.download(directory)
+        print('Download finished')
