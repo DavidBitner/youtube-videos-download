007cdf2179ce240f337b47d95230a8904e6e4107
David <david-bitner@hotmail.com>
2020-09-06 13:12:56 -0300

    Update youtube-videos-download.py

--- youtube-videos-download.py
+++ youtube-videos-download.py
@@ -1,41 +1,11 @@
-# DEVELOPING
-
-import tkinter as tk
-from tkinter import filedialog, Text
-import os
-import pytube
-
-
-def download_video(directory='C:/Users/David/Desktop/Videos', video_url='0'):
-    while True:
-        if video_url == '0':
-            break
-        youtube = pytube.YouTube(video_url)
-        video = youtube.streams.get_highest_resolution()
-        video.download(directory)
-
-
-def input_video_url():
-    video_url = input()
-    return video_url
-
+from tkinter import *
 
 # Main
-vu = input_video_url()
-root = tk.Tk()
+root = Tk()
+root.title("Youtube Videos Download")
+root.geometry("500x500+200+200")
 
-canvas = tk.Canvas(root, height=300, width=300, bg='black')
-canvas.pack()
-
-frame = tk.Frame(root, bg='white')
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-youtube_url_label = tk.Label(frame, text='Youtube URL: ', padx=10, pady=5, bg='gray')
-youtube_url_label.pack()
-test_label = tk.Label(frame, text=vu, bg='blue')
-test_label.pack()
-
-download = tk.Button(root, text='Download', padx=10, pady=5, fg='white', bg='black', command=download_video())
-download.pack()
+frame = LabelFrame(root, padx=5, pady=5)
+frame.pack(padx=100, pady=100)
 
 root.mainloop()
