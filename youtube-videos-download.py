83b68875185433d48b66f82dbc07407fc3ceb794
David <david-bitner@hotmail.com>
2020-09-08 11:33:20 -0300

    Update youtube-videos-download.py
    Created the download function

--- youtube-videos-download.py
+++ youtube-videos-download.py
@@ -21,6 +21,24 @@ def paste():
     video_link_entry.insert(0, pyperclip.paste())
 
 
+def download():
+    global status_label
+    video_url = video_link_entry.get()
+    status_label.destroy()
+    youtube = pytube.YouTube(video_url)
+    status_label = Label(frame, text=f"Downloading:  {youtube.title}")
+    status_label.grid(row=3, column=0, columnspan=2, pady=20, padx=10)
+    video = youtube.streams.get_highest_resolution()
+    directory = directory_label.cget("text")
+    video.download(directory)
+    youtube = pytube.YouTube(video_url)
+    status_label.destroy()
+    status_label = Label(frame, text=f"FINISHED: {youtube.title}")
+    status_label.grid(row=3, column=0, columnspan=2, pady=20, padx=10)
+    doidera = Label(frame2, text=youtube.title)
+    doidera.pack()
+
+
 # Main
 root = Tk()
 root.title("Youtube Videos Download")
@@ -28,6 +46,9 @@ root.geometry("500x500+100+100")
 
 frame = LabelFrame(root, padx=5, pady=5)
 frame.pack(padx=50, pady=50)
+
+frame2 = LabelFrame(frame, padx=5, pady=5)
+frame2.grid(row=4, column=0, columnspan=2)
 
 directory_label = Label(frame, text="Select folder")
 directory_label.grid(row=0, column=0)
@@ -38,16 +59,16 @@ directory_btn.grid(row=0, column=1, pady=10, padx=5)
 video_link_entry = Entry(frame, font="Helvetica 10")
 video_link_entry.grid(row=1, column=0, ipadx=40, ipady=5)
 
-paste_btn = Button(frame, text="PASTE", height=2, width=10, command=paste)
-paste_btn.grid(row=1, column=1, pady=10, padx=5)
+paste_link_btn = Button(frame, text="PASTE", height=2, width=10, command=paste)
+paste_link_btn.grid(row=1, column=1, pady=10, padx=5)
 
-download_btn = Button(frame, text="DOWNLOAD", height=2, width=20)
-download_btn.grid(row=2, column=0, pady=10, padx=5, columnspan=2)
+download_video_btn = Button(frame, text="DOWNLOAD", command=download, height=2, width=20)
+download_video_btn.grid(row=2, column=0, pady=10, padx=5, columnspan=2)
 
 status_label = Label(frame, text="Download status")
 status_label.grid(row=3, column=0, columnspan=2, pady=20, padx=10)
 
-downloaded_label = Label(frame, text="Downloaded videos:")
-downloaded_label.grid(row=4, column=0, columnspan=2, pady=20, padx=10)
+downloaded_label = Label(frame2, text="Downloaded videos:")
+downloaded_label.pack()
 
 root.mainloop()
