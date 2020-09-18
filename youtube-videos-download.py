ee75772754efeddd7b233824d23e8981a2c2d718
David <david-bitner@hotmail.com>
2020-09-18 15:23:16 -0300

    Update youtube-videos-download.py

--- youtube-videos-download.py
+++ youtube-videos-download.py
@@ -24,30 +24,39 @@ def paste():
 
 # Function to download videos
 def download():
+    # Imports for the thumbnail
     from PIL import ImageTk, Image
-    import os
     import requests
     from io import BytesIO
+
+    # Making the variable that represents the image global so the function can call the image on the GUI
     global img
+
+    # In order to the label that shows which video was downloaded to work without problems, this need to be made
     global status_label
     status_label.destroy()
+
+    # Download video
     video_url = video_link_entry.get()
     youtube = pytube.YouTube(video_url)
     video = youtube.streams.get_highest_resolution()
     directory = directory_label.cget("text")
     # video.download(directory)
     status_label = Label(frame, text=f"FINISHED: {youtube.title}")
-    status_label.grid(row=3, column=0, columnspan=2, pady=20, padx=10)
+    status_label.grid(row=3, column=0, columnspan=2, pady=5, padx=10)
 
     # Thumbnail
-    img_url = "https://i.ytimg.com/vi/ncDQruu4BbI/maxresdefault.jpg"
+    img_url = youtube.thumbnail_url
     response = requests.get(img_url)
     img_data = response.content
-    img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-    thumbnail_label = Label(frame2, image=img, height=300, width=300)
-    thumbnail_label.pack()
+    # img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    img = Image.open(BytesIO(img_data))
+    img = img.resize((300, 200), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    thumbnail_label = Label(frame, image=img, height=200, width=300)
+    thumbnail_label.grid(row=4, column=0, columnspan=2, pady=10, padx=10)
 
-    # Download videos names
+    # Increment the list of downloaded videos
     downloaded_video_label = Label(frame2, text=youtube.title)
     downloaded_video_label.pack()
 
@@ -55,13 +64,13 @@ def download():
 # Main
 root = Tk()
 root.title("Youtube Videos Download")
-root.geometry("500x500+100+100")
+root.geometry("700x700+100+100")
 
 frame = LabelFrame(root, padx=5, pady=5)
 frame.pack(padx=50, pady=50)
 
 frame2 = LabelFrame(frame, padx=5, pady=5)
-frame2.grid(row=4, column=0, columnspan=2)
+frame2.grid(row=5, column=0, columnspan=2)
 
 directory_label = Label(frame, text="Select folder")
 directory_label.grid(row=0, column=0)
